3d90d70808cba9e1a2e206083dbaad38ee1dd290
Abhirup-99 <abhiruppalmethodist@gmail.com>
2020-11-06 20:11:24 +0530

    Initital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
+import { TitlebarComponent } from './navigation/titlebar/titlebar.component';
+import { SharedModule } from './shared/shared.module';
+import { SidelinksComponent } from './navigation/sidebar/sidelinks/sidelinks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TitlebarComponent,
+    SidelinksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation/sidebar/sidelinks/sidelinks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidelinks',
+  templateUrl: './sidelinks.component.html',
+  styleUrls: ['./sidelinks.component.scss']
+})
+export class SidelinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
